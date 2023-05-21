ecc434560955899c458ea0ec894c010079c01e8c
letit6E <rustam.letitbe@gmail.com>
2023-05-21 21:23:50 +0300

    Reformat and comment AE Chunker

--- src/chunking/ae.rs
+++ src/chunking/ae.rs
@@ -3,18 +3,19 @@ use std::f64::consts;
 use std::io::{Read, Write};
 use std::ptr;
 
+// "AE: An Asymmetric Extremum Content Defined Chunking Algorithm for Fast and Bandwidth-Efficient Data Deduplication"
 pub struct AeChunker {
-    buffer: [u8; 0x1000],
-    left: usize,
-    window_size: usize,
+    buffer: [u8; 4096], // for buffered reading from input(4096 is max size of chunk)
+    buffered: usize,    // count of first already taken bytes
+    window_size: usize, // size for extremum window
 }
 
 impl AeChunker {
     pub fn new(avg_size: usize) -> AeChunker {
         AeChunker {
-            buffer: [0; 0x1000],
-            left: 0,
-            window_size: ((avg_size as f64) / (consts::E - 1.)).round() as usize,
+            buffer: [0; 4096],
+            buffered: 0,
+            window_size: ((avg_size as f64) / (consts::E - 1.)).round() as usize, // from paper
         }
     }
 }
@@ -22,48 +23,54 @@ impl AeChunker {
 impl Chunker for AeChunker {
     fn next_chunk(
         &mut self,
-        r: &mut dyn Read,
-        w: &mut dyn Write,
+        input: &mut dyn Read,
+        output: &mut dyn Write,
     ) -> Result<ChunkerStatus, ChunkerError> {
-        let mut global_pos: usize = 0;
         let mut max_pos: usize = 0;
         let mut max_val: u8 = 0;
         loop {
-            let mut max = r
-                .read(&mut self.buffer[self.left..])
+            // [0, self.buffered - 1] is already taken bytes
+            // max is end of data if it less than buffer size
+            let max = input
+                .read(&mut self.buffer[self.buffered..])
                 .map_err(ChunkerError::Read)?
-                + self.left;
+                + self.buffered;
 
+            // if end of data is equal to 0 then there is no data
             if max == 0 {
                 return Ok(ChunkerStatus::Finished);
             }
 
-            for i in 0..max {
-                let cur_val = self.buffer[i];
+            for chunk_pos in 0..max {
+                let cur_val = self.buffer[chunk_pos];
 
                 if cur_val > max_val {
                     max_val = cur_val;
-                    max_pos = global_pos;
-                } else if global_pos == max_pos + self.window_size {
-                    w.write_all(&self.buffer[i + 1..])
+                    max_pos = chunk_pos;
+                } else if chunk_pos == max_pos + self.window_size {
+                    // returning chunk to output stream
+                    output
+                        .write_all(&self.buffer[chunk_pos + 1..])
                         .map_err(ChunkerError::Write)?;
+                    // moving data to beginning of buffer(size of this data is equal to max-chunk_pos-1)
                     unsafe {
                         ptr::copy(
-                            self.buffer[i + 1..].as_ptr(),
+                            self.buffer[chunk_pos + 1..].as_ptr(),
                             self.buffer.as_mut_ptr(),
-                            max - i - 1,
+                            max - chunk_pos - 1,
                         )
                     };
-                    self.left = max - i - 1;
+                    self.buffered = max - chunk_pos - 1;
+
                     return Ok(ChunkerStatus::Working);
                 }
-
-                global_pos += 1;
             }
 
-            w.write_all(&self.buffer[..max])
-                .map_err(ChunkerError::Write);
-            self.left = 0;
+            // max size of chunk or end of input reached
+            output
+                .write_all(&self.buffer[..max])
+                .map_err(ChunkerError::Write)?;
+            self.buffered = 0;
         }
     }
 }
