cc5acab085e60d7d13aa5b436d06e1417a91aa21
letit6E <letit6E>
2023-10-11 11:05:25 +0300

    Add rayon and threads test for cdc algorithms

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod chunking;
 use chunking::{ae::*, chunker::*, fastcdc::*, rabin::*};
 use fastcdc::v2020::Chunk;
+use rayon::prelude::*;
 
 use std::collections::hash_map::DefaultHasher;
 use std::collections::HashSet;
@@ -8,7 +9,7 @@ use std::fs::File;
 use std::hash::{Hash, Hasher};
 use std::io::{BufReader, Cursor, Error, Read, Seek, SeekFrom};
 use std::thread;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 fn test_chunking(chunker: &mut dyn Chunker, data: &mut dyn Read) -> Vec<Vec<u8>> {
     let mut chunks: Vec<Vec<u8>> = vec![];
@@ -38,7 +39,7 @@ fn internal_chunking(path: &str, n: usize) -> Vec<Chunk> {
     chunks
 }
 
-fn fcdctest(path: &str, n: usize) -> Vec<Vec<u8>> {
+fn fcdc_test(path: &str, n: usize) -> Vec<Vec<u8>> {
     let f = File::open(path).unwrap();
     let mut reader = BufReader::new(&f);
     let mut chunker = FastCdcChunker::new(1024 * n, 1);
@@ -47,7 +48,7 @@ fn fcdctest(path: &str, n: usize) -> Vec<Vec<u8>> {
     result
 }
 
-fn aetest(path: &str, n: usize) -> Vec<Vec<u8>> {
+fn ae_test(path: &str, n: usize) -> Vec<Vec<u8>> {
     let f = File::open(path).unwrap();
     let mut reader = BufReader::new(&f);
     let mut chunker = AeChunker::new(1024 * n);
@@ -56,7 +57,7 @@ fn aetest(path: &str, n: usize) -> Vec<Vec<u8>> {
     result
 }
 
-fn rabintest(path: &str, n: usize) -> Vec<Vec<u8>> {
+fn rabin_test(path: &str, n: usize) -> Vec<Vec<u8>> {
     let f = File::open(path).unwrap();
     let mut reader = BufReader::new(&f);
     let mut chunker = RabinChunker::new(1024 * n, 0);
@@ -78,53 +79,79 @@ fn hash_vec<T: Hash>(v: &Vec<T>) -> u64 {
     v.hash(&mut hasher);
     hasher.finish()
 }
-fn main() {
-    let path = "./resources/test.mkv";
-    let n = 64usize;
-    let threads_cnt = 4usize;
 
-    // let now = Instant::now();
-    // let result = internal_chunking(path, n);
-    // let mut set = HashSet::new();
-    // let mut sm_data = 0;
-    // let mut sm_len = 0;
-    // for elem in &result {
-    //     sm_len += elem.length;
-    //     let hsh = elem.hash;
-    //     if !set.contains(&hsh) {
-    //         set.insert(hsh);
-    //         sm_data += elem.length;
-    //     }
-    // }
-    let now = Instant::now();
-    let result = aetest(path, n);
-    let mut set = HashSet::new();
-    let mut sm_data = 0;
-    let mut sm_len = 0;
-    for elem in &result {
-        sm_len += elem.len();
-        let hsh = hash_vec(&elem);
-        if !set.contains(&hsh) {
-            set.insert(hsh);
-            sm_data += elem.len();
-        }
-    }
-    let elapsed = now.elapsed();
-    
-    println!("=========\nTIME  = {:.3?}\n==========", elapsed);
-    println!("=========\nCOEFF = {:.3?}\n==========", (sm_data as f64) / (sm_len as f64));
-    println!("=========\nAVG   = {:.3?}\n==========", (sm_len as f64) / (result.len() as f64));
-    println!("=========\nFILE  = {:.3?}\n==========", sm_len);
-
-    return;
-
-    let buffer = read_file(path).unwrap();
+fn preprocess_vec(buffer: &Vec<u8>, threads_cnt: usize) -> Vec<Vec<u8>> {
     let mut vecs: Vec<Vec<u8>> = vec![Vec::new(); threads_cnt];
     for i in 0..threads_cnt {
         let left = i * buffer.len() / threads_cnt;
         let right = (i + 1) * buffer.len() / threads_cnt;
         vecs[i] = (buffer[left..right]).to_vec();
     }
+    return vecs;
+}
+
+fn work_for_thread(i: usize, n: usize, cur: &Vec<u8>) -> (HashSet<u64>, usize, Vec<u8>) {
+    let mut chunker = AeChunker::new(1024 * n);
+    // let mut chunker = FastCdcChunker::new(1024 * n, 1);
+    // let mut chunker = RabinChunker::new(1024 * n, 0);
+    let mut cursor = Cursor::new(cur);
+    let mut set = HashSet::new();
+    let mut result = 0;
+
+    let vecs = test_chunking(&mut chunker, &mut cursor);
+    let left = if i % 2 == 0 { 0 } else { 1 };
+    let right = if i % 2 == 0 {
+        vecs.len() - 1
+    } else {
+        vecs.len()
+    };
+
+    for i in left..right {
+        let hsh = hash_vec(&vecs[i]);
+        if !set.contains(&hsh) {
+            set.insert(hsh);
+            result += vecs[i].len();
+        }
+    }
+
+    let remainder = if i % 2 == 0 {
+        vecs.last()
+    } else {
+        vecs.first()
+    };
+    (set, result, remainder.unwrap().clone())
+}
+
+fn collect_results(
+    result_set: &mut HashSet<u64>,
+    mut ans: usize,
+    remainders: &mut Vec<Vec<u8>>,
+    n: usize,
+) -> (usize, usize) {
+    let mut i = 0;
+    while i < remainders.len() - 1 {
+        let mut t = remainders[i + 1].clone();
+        remainders[i].append(&mut t);
+        let mut chunker = AeChunker::new(1024 * n);
+        // let mut chunker = FastCdcChunker::new(1024 * n, 1);
+        // let mut chunker = RabinChunker::new(1024 * n, 0);
+        let mut cursor = Cursor::new(&remainders[i]);
+        for elem in test_chunking(&mut chunker, &mut cursor) {
+            let hsh = hash_vec(&elem);
+            if !result_set.contains(&hsh) {
+                result_set.insert(hsh);
+                ans += elem.len();
+            }
+        }
+        i += 2;
+    }
+
+    (ans, result_set.len())
+}
+
+fn threads_test(path: &str, n: usize, threads_cnt: usize) -> (Duration, f64, f64) {
+    let buffer = read_file(path).unwrap();
+    let vecs = preprocess_vec(&buffer, threads_cnt);
 
     let now = Instant::now();
     let (len, cnt) = {
@@ -132,35 +159,7 @@ fn main() {
 
         for i in 0..threads_cnt {
             let cur = vecs[i].clone();
-            threads.push(thread::spawn(move || {
-                let mut chunker = RabinChunker::new(1024 * n, 0);
-                let mut cursor = Cursor::new(cur);
-                let mut set = HashSet::new();
-                let mut result = 0;
-
-                let vecs = test_chunking(&mut chunker, &mut cursor);
-                let left = if i % 2 == 0 { 0 } else { 1 };
-                let right = if i % 2 == 0 {
-                    vecs.len() - 1
-                } else {
-                    vecs.len()
-                };
-
-                for i in left..right {
-                    let hsh = hash_vec(&vecs[i]);
-                    if !set.contains(&hsh) {
-                        set.insert(hsh);
-                        result += vecs[i].len();
-                    }
-                }
-
-                let remainder = if i % 2 == 0 {
-                    vecs.last()
-                } else {
-                    vecs.first()
-                };
-                (set, result, remainder.unwrap().clone())
-            }));
+            threads.push(thread::spawn(move || work_for_thread(i, n, &cur)));
         }
 
         let mut result_set: HashSet<u64> = HashSet::new();
@@ -173,40 +172,54 @@ fn main() {
             ans += x;
         }
 
-        let mut i = 0;
-        while i < remainders.len() - 1 {
-            let mut t = remainders[i + 1].clone();
-            remainders[i].append(&mut t);
-            let mut chunker = RabinChunker::new(1024 * n, 0);
-            let mut cursor = Cursor::new(&remainders[i]);
-            for elem in test_chunking(&mut chunker, &mut cursor) {
-                let hsh = hash_vec(&elem);
-                if !result_set.contains(&hsh) {
-                    result_set.insert(hsh);
-                    ans += elem.len();
-                }
-            }
-            i += 2;
-        }
-
-        (ans, result_set.len())
+        collect_results(&mut result_set, ans, &mut remainders, n)
     };
+
     let elapsed = now.elapsed();
-    println!(
-        "=========\nTIME = {:.3?}, COEFF = {}, AVG SIZE = {} \n==========",
+    return (
         elapsed,
         (len as f64) / (buffer.len() as f64),
-        (len as f64) / (cnt as f64)
+        (len as f64) / (cnt as f64),
     );
-    // let internal_result = internal_chunking(n, path);
-    // let fcdc_result = fcdctest(path, n);
-    // let ae_result = aetest(path, n);
-    // let rabin_result = rabintest(path, n);
-    // println!(
-    //     "{} {} {} {}",
-    //     internal_result.len(),
-    //     fcdc_result.len(),
-    //     ae_result.len(),
-    //     rabin_result.len()
-    // );
+}
+
+fn rayon_test(path: &str, n: usize, threads_cnt: usize) -> (Duration, f64, f64) {
+    let buffer = read_file(path).unwrap();
+    let mut vecs = preprocess_vec(&buffer, threads_cnt);
+
+    let now = Instant::now();
+    let (len, cnt) = {
+        let tmp: Vec<(HashSet<u64>, usize, Vec<u8>)> = (0..threads_cnt)
+            .into_par_iter()
+            .map(|i| {
+                let cur = vecs[i].clone();
+                work_for_thread(i, n, &cur)
+            })
+            .collect();
+
+        let mut result_set: HashSet<u64> = HashSet::new();
+        let mut ans = 0;
+        let mut remainders = vec![];
+        for handle in tmp {
+            let (st, x, y) = handle;
+            result_set.extend(&st);
+            remainders.push(y);
+            ans += x;
+        }
+
+        collect_results(&mut result_set, ans, &mut remainders, n)
+    };
+
+    let elapsed = now.elapsed();
+    return (
+        elapsed,
+        (len as f64) / (buffer.len() as f64),
+        (len as f64) / (cnt as f64),
+    );
+}
+
+fn main() {
+    let path = "./resources/valvesockets.tar";
+    let n = 8usize;
+    let threads_cnt = 3usize;
 }
